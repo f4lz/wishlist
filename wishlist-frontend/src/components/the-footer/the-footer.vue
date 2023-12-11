f8ca5b75dd2f547ee9ce3c4541b0d443a7a4a3ab
Igor Rogoza <krab336@mail.ru>
2023-12-11 23:30:06 +0300

    исправления ui-kit

--- wishlist-frontend/src/components/the-footer/the-footer.vue
+++ wishlist-frontend/src/components/the-footer/the-footer.vue
@@ -1,8 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-    <div class="flex text-center rounded-t-lg">
-      <h6>Podnojka.ru</h6>
-    </div>
+  <div class="flex text-center rounded-t-lg">
+    <h6>Podnojka.ru</h6>
+  </div>
 </template>
- 
--- wishlist-frontend/src/pages/pageNotFound.vue
+++ wishlist-frontend/src/pages/pageNotFound.vue
@@ -3,9 +3,9 @@
 <template>
   <div class="bg-indigo-600 w-full h-full flex justify-center items-center">
     <div class="flex flex-col items-center gap-y-3 text-secondary">
-      <h3>Ошибка</h3>
-      <h2>404</h2>
-      <h2>Старница не найдена</h2>
+      <h4>Ошибка</h4>
+      <h1>404</h1>
+      <h1>Старница не найдена</h1>
       <router-link class="button" to="/">
         Вернуться на главную страницу
       </router-link>
