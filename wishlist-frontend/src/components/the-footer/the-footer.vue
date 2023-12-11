036b4708acccbf66b3f3ff4ce3fe99eb136befaf
Igor Rogoza <krab336@mail.ru>
2023-12-11 23:32:15 +0300

    контейнер для футера

--- wishlist-frontend/src/components/the-footer/the-footer.vue
+++ wishlist-frontend/src/components/the-footer/the-footer.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="flex text-center rounded-t-lg">
+  <div class="flex text-center container rounded-t-lg">
     <h6>Podnojka.ru</h6>
   </div>
 </template>
