58b4397ebdc212dac55f8c9fbb7387dc99443847
Igor Kurapov <igorkurapov99@gmail.com>
2023-12-13 02:54:46 +0300

    Изменения страницы 404

--- wishlist-frontend/src/pages/pageNotFound.vue
+++ wishlist-frontend/src/pages/pageNotFound.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="bg-indigo-600 w-full h-full flex justify-center items-center">
-    <div class="flex flex-col items-center gap-y-3 text-secondary">
+  <div class="bg-teal-300 w-full h-full flex justify-center items-center">
+    <div class="flex flex-col items-center gap-y-3 text-primary">
       <h4>Ошибка</h4>
       <h1>404</h1>
       <h1>Старница не найдена</h1>
-      <router-link class="button" to="/">
+      <router-link class="button p-3 border-2 border-black rounded-xl" to="/">
         Вернуться на главную страницу
       </router-link>
     </div>
