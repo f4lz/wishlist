24a9e472b38fd8e4386ba496376490831cae8e38
Igor Kurapov <igorkurapov99@gmail.com>
2023-12-13 03:41:35 +0300

    Верстка кнопки на странице Мои подарки

--- wishlist-frontend/src/components/profile/profile-gifts/profile-gifts.vue
+++ wishlist-frontend/src/components/profile/profile-gifts/profile-gifts.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import type { UiContent } from '@/types/main'
+import { Icon } from '@iconify/vue';
 
 const gifts: UiContent[] = [
   {
@@ -8,23 +9,19 @@ const gifts: UiContent[] = [
   },
   {
     id: '1212212',
-    title: 'Аня',
+    title: 'Шаурман',
   },
   {
     id: '1228712',
-    title: 'Аня',
+    title: 'Телевизор',
   },
   {
     id: '121212',
-    title: 'Аня',
+    title: 'Валына',
   },
   {
     id: '121212111',
-    title: 'Аня',
-  },
-  {
-    id: '121212321322',
-    title: 'Аня',
+    title: 'Шарф',
   },
 ]
 </script>
@@ -32,6 +29,12 @@ const gifts: UiContent[] = [
 <template>
   <div>
     <ui-grid>
+      <div class="bg-background-secondary h-3/6 flex flex-col justify-center items-center drop-shadow hover:drop-shadow-2xl rounded-xl transition">
+        <button class="flex flex-col items-center">
+          <Icon class="!w-10 !h-10 shrink-0" icon="material-symbols:add-circle" />
+          <p>Добавить подарок</p>
+        </button>
+      </div>
       <ui-card v-for="gift of gifts" :key="gift.id" :card="gift" is-gift />
     </ui-grid>
   </div>
