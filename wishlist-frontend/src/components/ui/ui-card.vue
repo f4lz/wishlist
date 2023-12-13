520ad032677bccaf8bc6251c268be62427d0aea3
Igor Rogoza <krab336@mail.ru>
2023-12-13 17:22:13 +0300

    Исправления кода

--- wishlist-frontend/src/components/ui/ui-card.vue
+++ wishlist-frontend/src/components/ui/ui-card.vue
@@ -2,8 +2,6 @@
 import { useContextMenu } from '@/stores/context'
 import type { UiContent } from '@/types/main'
 import { Icon } from '@iconify/vue'
-import { onClickOutside } from '@vueuse/core'
-import type { Ref } from 'vue'
 
 const props = defineProps<{
   card: UiContent
@@ -18,15 +16,18 @@ const onRemove = () => {
   console.log('remove')
 }
 
-const context: Ref<HTMLDivElement> = ref()
-
 const contextMenu = useContextMenu()
 
-const isMenuOpen = computed(() => props.card.id === contextMenu.openedId)
+const toggleMenu = () => {
+  console.log(contextMenu.openedId)
+  if (contextMenu.openedId !== null) {
+    console.log('dada')
+    return (contextMenu.openedId = null)
+  }
+  contextMenu.openedId = props.card.id
+}
 
-onClickOutside(context, () => {
-  contextMenu.openedId = null
-})
+const isMenuOpen = computed(() => props.card.id === contextMenu.openedId)
 </script>
 
 <template>
@@ -37,9 +38,8 @@ onClickOutside(context, () => {
     ]">
     <button
       v-if="isGift"
-      ref="context"
       class="absolute right-0 top-0 translate-y-1/2 -translate-x-1/2 hover:bg-background-secondary transition rounded-full"
-      @click="contextMenu.openedId = card.id">
+      @click="toggleMenu">
       <Icon class="!w-6 !h-6 shrink-0" icon="pepicons-pencil:dots-y" />
     </button>
     <img v-if="card.img" :alt="card.title" :src="card.img" />
