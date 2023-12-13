6e442bb41f1bf0e92de2e7bee3b9bbadb24bc724
Bo4kaa <kirilldadi182@gmail.com>
2023-12-13 23:34:32 +0300

    сделала профиль кроме кнопки

--- wishlist-frontend/src/components/profile/profile-settings/profile-settings.vue
+++ wishlist-frontend/src/components/profile/profile-settings/profile-settings.vue
@@ -1,10 +1,20 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="flex justify-center">
+  <div class="flex gap-7">
     <div class="flex flex-col align-center w-2/4 gap-y-4">
       <div class="w-[150px] h-[150px] bg-black rounded-full"></div>
-      <button class="button-primary p-auto text-center">Изменить</button>
+      <input class="text-[18px] border-1 rounded px-0 "  type="file">
+    </div>
+    <div class="flex flex-col gap-y-16">
+      <div>
+        <span>Изменить имя</span>
+        <input class="w-[465px] h-[39px] border border-black" placeholder="введите имя" type="text"/>
+      </div>
+      <div>
+        <span>Новый пароль</span>
+        <input class="w-[465px] h-[39px] border border-black" placeholder="введите пароль" type="text"/>
+    </div>
     </div>
   </div>
 </template>
